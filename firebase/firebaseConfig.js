faa76d416613181bfec46c8e564054c329964a16
sriramya <sriramyavijjana@gmail.com>
2025-06-24 13:28:10 +0530

    Switch to using FIREBASE_CONFIG environment variable

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -1,7 +1,6 @@
-// firebase/firebaseConfig.js
-
 const admin = require('firebase-admin');
-const serviceAccount = require('../serviceAccountKey.json');
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
